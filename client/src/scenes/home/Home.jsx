5424a1324e34e1158dde90c6abfd9201b3d21a52
c0descrub <jordanjames1095@googlemail.com>
2022-12-13 10:19:35 +0000

    completed hero section carousel

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,7 +1,9 @@
+import MainCarousel from "./MainCarousel";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <MainCarousel />
     </div>
   );
 };
